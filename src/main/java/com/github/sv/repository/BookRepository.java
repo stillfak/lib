7b24267999750c8298e231da4dba7eafe5e295e6
vadim <vadim.gavrikov.1998@gmail.com>
2018-05-18 15:35:56 +0300

    add abstract method

--- src/main/java/com/github/sv/repository/BookRepository.java
+++ src/main/java/com/github/sv/repository/BookRepository.java
@@ -4,8 +4,10 @@ import com.github.sv.models.Book;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
     List<Book> findByBookName(String bookName);
+    Optional<Book> findById(Long id);
 }
