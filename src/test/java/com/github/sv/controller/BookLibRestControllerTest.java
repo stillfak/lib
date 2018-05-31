93c3e5da209366fe3bb64c091531761f43595034
vadim <vadim.gavrikov.1998@gmail.com>
2018-05-31 17:01:12 +0300

    implemented and successfully passed Unit tests for methods getAllBooks and getBook

--- src/test/java/com/github/sv/controller/BookLibRestControllerTest.java
+++ src/test/java/com/github/sv/controller/BookLibRestControllerTest.java
@@ -1,9 +1,12 @@
 package com.github.sv.controller;
 
+import com.github.sv.LibApplication;
 import com.github.sv.dto.BookDTO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -16,10 +19,11 @@ import static org.junit.Assert.assertEquals;
 @ActiveProfiles("test")
 @Transactional
 @Rollback
+@SpringBootTest(classes = LibApplication.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 public class BookLibRestControllerTest {
 
-
+    @Autowired
     private BookLibRestController libRestController;
 
 //    @Autowired
@@ -30,19 +34,28 @@ public class BookLibRestControllerTest {
     @Test
     public void getAllBooks() {
         SpringDataWebProperties.Pageable pageable = new SpringDataWebProperties.Pageable();
-
         ArrayList<BookDTO> books = (ArrayList<BookDTO>) libRestController.getAllBooks(pageable);
-        ArrayList<BookDTO> bookDTOS = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
+            assertEquals(books.get(i).getAuthorBook(),"author"+i);
+            assertEquals(books.get(i).getBookName(),"book"+i);
+            assertEquals(Math.toIntExact(books.get(i).getId()) ,i+1);
+            assertEquals(Math.toIntExact(books.get(i).getNumberOfPages()), 600+i);
+            assertEquals(books.get(i).getAvailability(),false);
 //                bookRepository.save(new Book("book" + i, (long) 600 + i, "author" + i));
-             bookDTOS.add(new BookDTO("book" + i, (long) 600 + i, "author" + i));
+
         }
-        assertEquals(books, bookDTOS);
+
 
     }
 
     @Test
     public void getBook() {
+        BookDTO bookDTO = libRestController.getBook((long)3);
+        assertEquals(bookDTO.getAuthorBook(),"author2");
+        assertEquals(bookDTO.getBookName(),"book2");
+        assertEquals(Math.toIntExact(bookDTO.getId()) ,3);
+        assertEquals(Math.toIntExact(bookDTO.getNumberOfPages()), 602);
+        assertEquals(bookDTO.getAvailability(),false);
     }
 
     @Test
