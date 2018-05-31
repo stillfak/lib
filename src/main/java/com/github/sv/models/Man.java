63fc346c233cb8984449e6f6feda53d0e680df1b
vadim <vadim.gavrikov.1998@gmail.com>
2018-05-31 15:39:28 +0300

    added getters and setters

--- src/main/java/com/github/sv/models/Man.java
+++ src/main/java/com/github/sv/models/Man.java
@@ -7,33 +7,51 @@ import java.util.List;
 public class Man {
 
     @Id
-    @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String lastName;
+
     @Lob
     private List<Book> booksOnHand; // books id
 
 
-    protected Man() {}
+    protected Man() {
+    }
 
     public Man(String lastName) {
-//        this.booksOnHand = book;
         this.lastName = lastName;
 
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public List<Book> getBooksOnHand() {
+        return booksOnHand;
     }
 
     public void setBooksOnHand(List<Book> booksOnHand) {
         this.booksOnHand = booksOnHand;
     }
-//    public void setBooksOnHand(Book booksOnHand) {
-//        this.booksOnHand = booksOnHand;
-//    }
 
     @Override
     public String toString() {
-        return lastName + " id = " + id + ", count on hand = "+ booksOnHand.size();// + ", count books on hand = " + booksOnHand
+        return lastName + " id = " + id + ", count on hand = " + booksOnHand.size();// + ", count books on hand = " + booksOnHand
     }
 
 }
