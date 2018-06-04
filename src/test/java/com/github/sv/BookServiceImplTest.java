a2b823cb58731f9c95bd0d1e0af92b15bdc5ab2e
vadim <vadim.gavrikov.1998@gmail.com>
2018-06-04 10:23:12 +0300

    an implementation for testing the find method has been added

--- src/test/java/com/github/sv/BookServiceImplTest.java
+++ src/test/java/com/github/sv/BookServiceImplTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @ActiveProfiles("test")
 @Transactional
@@ -19,29 +19,42 @@ import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = LibApplication.class)
 public class BookServiceImplTest {
-
     @Autowired
     private BookServiceImpl bookService;
 
     @Test
     public void add() {
-        Book book = bookService.add(new Book("book",(long) 500,"author"));
-        assertEquals(book.getBookName(),"book");
-        assertEquals(book.getAuthorBook(),"author");
-        assertEquals(Math.toIntExact(book.getId()),1);
-        assertEquals(book.getAvailability(),false);
-        assertEquals(Math.toIntExact(book.getNumberOfPages()),500);
+        Book book = bookService.add(new Book("book", (long) 500, "author"));
+        assertEquals(bookService.getRepository().count(), 1);
+        assertEquals(book.getBookName(), "book");
+        assertEquals(book.getAuthorBook(), "author");
+        assertEquals(Math.toIntExact(book.getId()), 1);
+        assertEquals(book.getAvailability(), false);
+        assertEquals(Math.toIntExact(book.getNumberOfPages()), 500);
 
 
     }
 
     @Test
     public void delete() {
-
+        Book book = bookService.add(new Book("book", (long) 500, "author"));
+        assertEquals(bookService.getRepository().count(), 1);
+        bookService.delete(book);
+        assertEquals(bookService.getRepository().count(), 0);
     }
 
     @Test
     public void find() {
+        for (int i = 0; i < 10; i++) {
+            bookService.add(new Book("book" + i, (long) 500 + i, "author" + i));
+        }
+
+        bookService.find("book1").stream().forEach(book -> {
+            assertEquals(book.getAuthorBook(),"author1");
+            assertEquals(book.getBookName(),"book1");
+            assertEquals(Math.toIntExact(book.getNumberOfPages()),501);
+            assertEquals(book.getAvailability(),false);
+        });
 
     }
 
