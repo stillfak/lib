443a1f8860413af01962fe312368b07094154450
vadim <vadim.gavrikov.1998@gmail.com>
2018-06-05 12:29:27 +0300

    fixed the problem with object conversion

--- src/main/java/com/github/sv/controller/BookLibRestController.java
+++ src/main/java/com/github/sv/controller/BookLibRestController.java
@@ -3,11 +3,8 @@ package com.github.sv.controller;
 import com.github.sv.dto.BookDTO;
 import com.github.sv.models.Book;
 import com.github.sv.service.impl.BookServiceImpl;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,37 +16,34 @@ public class BookLibRestController {
 
     private final BookServiceImpl service;
 
-    private ModelMapper modelMapper;
-
+    private com.github.sv.Mapper.ModelMapper mapper;
 
 
     @Autowired
     public BookLibRestController(BookServiceImpl service) {
         this.service = service;
-        this.modelMapper = new ModelMapper();
-//        this.modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        this.mapper = new com.github.sv.Mapper.ModelMapper();
     }
-//    public BookLibRestController() {}
 
     BookServiceImpl getService() {
         return service;
     }
 
     @RequestMapping(value = "/books", method = RequestMethod.GET)
-    public List<BookDTO> getAllBooks(SpringDataWebProperties.Pageable pageable) {
-        return service.getRepository().findAll(/**pageable.getMaxPageSize()*/).stream().map(this::convertToDto).collect(Collectors.toList());
+    public List<BookDTO> getAllBooks() {//SpringDataWebProperties.Pageable pageable
+        return service.getRepository().findAll().stream().map(book -> mapper.convertToDto(book)).collect(Collectors.toList());
     }
 
     @RequestMapping(value = "/books/{id}", method = RequestMethod.GET)
     public BookDTO getBook(@PathVariable Long id) {
-        return convertToDto(service.findById(id).get());
+        return mapper.convertToDto(service.findById(id).get());
     }
 
     @RequestMapping(value = "/books", method = RequestMethod.POST)
     public BookDTO add(@RequestBody String name,
                        @RequestBody Long size,
                        @RequestBody String author) {
-        return convertToDto(service.add(new Book(name, size, author)));
+        return mapper.convertToDto(service.add(new Book(name, size, author)));
 
     }
 
@@ -64,21 +58,12 @@ public class BookLibRestController {
         service.findById(id).get().setNumberOfPages(size);
         service.findById(id).get().setAvailability(availability);
 
-
-        return convertToDto(service.findById(id).get());
+        return mapper.convertToDto(service.findById(id).get());
     }
 
     @RequestMapping(value = "/books/{id}", method = RequestMethod.DELETE)
     public BookDTO delete(@PathVariable Long id) {
-        return convertToDto(service.delete(service.findById(id).get()));
-    }
-
-    private BookDTO convertToDto(Book book) {
-        return modelMapper.map(book, BookDTO.class);
-    }
-
-    private Book convertToEnable(BookDTO sourceObject) {
-        return modelMapper.map(sourceObject, Book.class);
+        return mapper.convertToDto(service.delete(service.findById(id).get()));
     }
 
 
