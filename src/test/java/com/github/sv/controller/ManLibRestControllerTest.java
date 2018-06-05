65f78aba4da16fda8fc57a8fa1d2ed34478579f7
vadim <vadim.gavrikov.1998@gmail.com>
2018-06-05 12:47:47 +0300

    Created class testing class ManLibRestController

---
+++ src/test/java/com/github/sv/controller/ManLibRestControllerTest.java
@@ -0,0 +1,32 @@
+package com.github.sv.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ManLibRestControllerTest {
+
+    @Test
+    public void getAllMan() {
+    }
+
+    @Test
+    public void getMan() {
+    }
+
+    @Test
+    public void getBooksOnHand() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void edit() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
