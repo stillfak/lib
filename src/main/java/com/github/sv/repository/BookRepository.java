143b97be67227dd25178c6b3bc7492673df62309
vadim <vadim.gavrikov.1998@gmail.com>
2018-06-05 12:32:45 +0300

    changed the class's parent

--- src/main/java/com/github/sv/repository/BookRepository.java
+++ src/main/java/com/github/sv/repository/BookRepository.java
@@ -1,13 +1,16 @@
 package com.github.sv.repository;
 
 import com.github.sv.models.Book;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findByBookName(String bookName);
+
     Optional<Book> findById(Long id);
+
+//    List<Book> findAll(int maxPageSize);
 }
--- src/main/java/com/github/sv/repository/ManRepository.java
+++ src/main/java/com/github/sv/repository/ManRepository.java
@@ -2,12 +2,12 @@ package com.github.sv.repository;
 
 
 import com.github.sv.models.Man;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 
-public interface ManRepository extends CrudRepository<Man, Long> {
+public interface ManRepository extends JpaRepository<Man, Long> {
 
     List<Man> findByLastName(String lastName);
 }
