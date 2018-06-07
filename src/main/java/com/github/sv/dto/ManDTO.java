8e929cfcfd644927b5661c02cd9e0310a3405489
vadim <vadim.gavrikov.1998@gmail.com>
2018-06-07 12:56:18 +0300

    added getter and setter

--- src/main/java/com/github/sv/dto/ManDTO.java
+++ src/main/java/com/github/sv/dto/ManDTO.java
@@ -1,36 +1,53 @@
 package com.github.sv.dto;
 
-import com.github.sv.models.Book;
-
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ManDTO {
+public class ManDTO{
     @Id
-    @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String lastName;
 
     @Lob
-    private List<Book> booksOnHand; // books id
+    private List<BookDTO> booksOnHand; // books id
 
 
     protected ManDTO() {}
 
     public ManDTO(String lastName) {
-//        this.booksOnHand = book;
+        this.booksOnHand = new ArrayList<>();
         this.lastName = lastName;
-
     }
 
-    public void setBooksOnHand(List<Book> booksOnHand) {
+    public Long getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public List<BookDTO> getBooksOnHand() {
+        return booksOnHand;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBooksOnHand(List<BookDTO> booksOnHand) {
         this.booksOnHand = booksOnHand;
     }
-//    public void setBooksOnHand(Book booksOnHand) {
-//        this.booksOnHand = booksOnHand;
-//    }
 
     @Override
     public String toString() {
