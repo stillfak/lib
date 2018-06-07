93a0eef37f3e1fd7f83bab3ed45ef23b61b20637
vadim <vadim.gavrikov.1998@gmail.com>
2018-06-07 11:39:19 +0300

    implemented test method for the getAllMan method

--- src/test/java/com/github/sv/controller/ManLibRestControllerTest.java
+++ src/test/java/com/github/sv/controller/ManLibRestControllerTest.java
@@ -1,13 +1,38 @@
 package com.github.sv.controller;
 
+import com.github.sv.LibApplication;
+import com.github.sv.dto.ManDTO;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
+
+@ActiveProfiles("test")
+@Transactional
+@Rollback
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = LibApplication.class)
 public class ManLibRestControllerTest {
+
+    @Autowired
+    private ManLibRestController controller;
+
 
     @Test
     public void getAllMan() {
+        for (int i = 0; i < 10; i++) {
+            controller.add("Man"+i);
+        }
+        assertEquals(controller.getAllMan().size(),10);
+
     }
 
     @Test
