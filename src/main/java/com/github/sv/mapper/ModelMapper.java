86c5d965ca2469e07f43216e0543474c4d71905b
vadim <vadim.gavrikov.1998@gmail.com>
2018-06-15 11:03:39 +0300

    reworked class ModelMapper

--- src/main/java/com/github/sv/mapper/ModelMapper.java
+++ src/main/java/com/github/sv/mapper/ModelMapper.java
@@ -6,27 +6,102 @@ import com.github.sv.models.Book;
 import com.github.sv.models.Man;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
 @Component
 public class ModelMapper {
 
-    private org.modelmapper.ModelMapper modelMapper;
 
     public ModelMapper() {
-        this.modelMapper = new org.modelmapper.ModelMapper();
     }
 
     public ManDTO convertToDto(Man man) {
-        return modelMapper.map(man, ManDTO.class);
+        if (man.getBooksOnHand() == null) {
+            return new ManDTO(
+                    man.getLastName(),
+                    man.getId());
+        } else if (man.getId() == null) {
+            return new ManDTO(
+                    man.getLastName(),
+                    man.getBooksOnHand().
+                            stream().
+                            map(this::convertToDto).
+                            collect(Collectors.toList()));
+        } else if (man.getId() == null && man.getBooksOnHand() == null) {
+            return new ManDTO(man.getLastName());
+        }
+
+        return new ManDTO(
+                man.getLastName(),
+                man.getBooksOnHand().
+                        stream().
+                        map(this::convertToDto).
+                        collect(Collectors.toList()),
+                man.getId());
     }
 
     public Man convertToEnable(ManDTO sourceObject) {
-        return modelMapper.map(sourceObject, Man.class);
-    }
-    public BookDTO convertToDto(Book book) {
-        return modelMapper.map(book, BookDTO.class);
+
+        if (sourceObject.getBooksOnHand() == null) {
+            return new Man(
+                    sourceObject.getLastName(),
+                    sourceObject.getId());
+        } else if (sourceObject.getId() == null) {
+            return new Man(
+                    sourceObject.getLastName(),
+                    sourceObject.
+                            getBooksOnHand().
+                            stream().
+                            map(this::convertToEnable).
+                            collect(Collectors.toList()));
+        } else if (sourceObject.getId() == null && sourceObject.getBooksOnHand() == null) {
+            return new Man(sourceObject.getLastName());
+        }
+        return new Man(
+                sourceObject.getLastName(),
+                sourceObject.
+                        getBooksOnHand().
+                        stream().
+                        map(this::convertToEnable).
+                        collect(Collectors.toList()),
+                sourceObject.getId());
     }
 
-    public Book convertToEnable(BookDTO sourceObject) {
-        return modelMapper.map(sourceObject, Book.class);
+
+    public BookDTO convertToDto(Book book) {
+        if (book.getMan() == null) {
+            return new BookDTO(book.getBookName(),
+                    book.getNumberOfPages(),
+                    book.getAuthorBook(),
+                    book.getId());
+
+        } else if (book.getId() == null) {
+            return new BookDTO(book.getBookName(),
+                    book.getNumberOfPages(),
+                    book.getAuthorBook());
+        }
+        return new BookDTO(book.getBookName(),
+                book.getNumberOfPages(),
+                book.getAuthorBook(),
+                book.getId(),
+                convertToDto(book.getMan()));
+    }
+
+    public Book convertToEnable(BookDTO bookDTO) {
+        if (bookDTO.getManDTO() == null) {
+            return new Book(bookDTO.getBookName(),
+                    bookDTO.getNumberOfPages(),
+                    bookDTO.getAuthorBook(), bookDTO.getId());
+        } else if (bookDTO.getId() == null) {
+            return new Book(bookDTO.getBookName(),
+                    bookDTO.getNumberOfPages(),
+                    bookDTO.getAuthorBook());
+        }
+        return new Book(bookDTO.getBookName(),
+                bookDTO.getNumberOfPages(),
+                bookDTO.getAuthorBook(),
+                bookDTO.getId(),
+                convertToEnable(bookDTO.getManDTO()));
+
     }
 }
