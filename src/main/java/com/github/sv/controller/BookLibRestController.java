aebcb58fd85c05fb9eadd95b9eb852d72e6328ed
vadim <vadim.gavrikov.1998@gmail.com>
2018-06-19 14:40:14 +0300

    refactored and added a page for the getAllBooks method

--- src/main/java/com/github/sv/controller/BookLibRestController.java
+++ src/main/java/com/github/sv/controller/BookLibRestController.java
@@ -4,6 +4,7 @@ import com.github.sv.dto.BookDTO;
 import com.github.sv.mapper.ModelMapper;
 import com.github.sv.service.impl.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,53 +12,58 @@ import java.util.stream.Collectors;
 
 
 @RestController
-@RequestMapping("lib/libBooks")
+@RequestMapping("lib/books")
 public class BookLibRestController {
 
     private final BookServiceImpl service;
 
-    private ModelMapper mapper;
+    private final ModelMapper mapper;
 
 
     @Autowired
-    public BookLibRestController(BookServiceImpl service) {
+    public BookLibRestController(BookServiceImpl service, ModelMapper mapper) {
         this.service = service;
-        this.mapper = new ModelMapper();
+        this.mapper = mapper;
     }
 
 
 
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
-    public List<BookDTO> getAllBooks() {//SpringDataWebProperties.Pageable pageable
-        return service.findAll()
+    @RequestMapping(method = RequestMethod.GET)
+    public List<BookDTO> getAllBooks(Pageable pageable) {//SpringDataWebProperties.
+        return service.findAll(pageable)
                 .stream()
                 .map(mapper::convertToDto)
                 .collect(Collectors.toList());
     }
 
-    @RequestMapping(value = "/books/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public BookDTO getBook(@PathVariable Long id) {
         return mapper.convertToDto(service.findById(id));
     }
 
-    @RequestMapping(value = "/books", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public BookDTO add(@RequestBody BookDTO newBook) {
         return mapper.convertToDto(service.add(mapper.convertToEnable(newBook)));
     }
 
-    @RequestMapping(value = "/books/{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public BookDTO edit(@PathVariable Long id,
                         @RequestBody BookDTO editBook) {
         return mapper.convertToDto(service.update(mapper.convertToEnable(editBook)));
     }
 
-    @RequestMapping(value = "/books/{id}", method = RequestMethod.DELETE)
-    public BookDTO delete(@PathVariable Long id) {
-        return mapper.convertToDto(service.deleteById(id));
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String delete(@PathVariable Long id) {
+        try {
+            service.deleteById(id);
+            return "Успешно";
+        }catch (Exception e){
+            return "Не найдено";
+        }
     }
 
-//    @RequestMapping(value = "/books", method = RequestMethod.GET)
-    public long getNumElemDB(){
-        return service.getRepositoryCount();
+    @RequestMapping(value = "/count", method = RequestMethod.GET)
+    public long count(){
+        return service.count();
     }
 }
--- src/main/java/com/github/sv/controller/ManLibRestController.java
+++ src/main/java/com/github/sv/controller/ManLibRestController.java
@@ -1,59 +1,70 @@
 package com.github.sv.controller;
 
 import com.github.sv.dto.ManDTO;
+import com.github.sv.exception.NotFoundException;
 import com.github.sv.mapper.ModelMapper;
 import com.github.sv.service.impl.ManServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("lib/libMans")
+@RequestMapping("lib/mans")
 public class ManLibRestController {
 
     private final ManServiceImpl service;
 
-    private ModelMapper mapper;
+    private final ModelMapper mapper;
 
     @Autowired
-    public ManLibRestController(ManServiceImpl service) {
+    public ManLibRestController(ManServiceImpl service, ModelMapper mapper) {
         this.service = service;
-        this.mapper = new ModelMapper();
+        this.mapper = mapper;
     }
 
-    @RequestMapping(value = "/mans", method = RequestMethod.GET)
-    public List<ManDTO> getAllMan() {//SpringDataWebProperties.Pageable pageable
-        return service.findAll().stream().map(mapper::convertToDto).collect(Collectors.toList());
+    @RequestMapping(method = RequestMethod.GET)
+    public List<ManDTO> getAllMan(Pageable pageable) {//
+        return service.findAll(pageable)
+                .stream()
+                .map(mapper::convertToDto)
+                .collect(Collectors.toList());
     }
 
-    @RequestMapping(value = "/mans/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ManDTO getMan(@PathVariable Long id) {
-        return mapper.convertToDto(service.findById(id).orElseThrow(() -> new UsernameNotFoundException("Username not found")));
+        return mapper.convertToDto(service.findById(id));
     }
 
-    @RequestMapping(value = "/mans", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public ManDTO add(@RequestBody ManDTO newMan) {
         return mapper.convertToDto(service.add(mapper.convertToEnable(newMan)));
     }
 
-    @RequestMapping(value = "/mans/{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ManDTO edit(@PathVariable Long id,
                        @RequestBody ManDTO editMan) {
         editMan.setId(id);
-        return mapper.convertToDto(service.update(mapper.convertToEnable(editMan)));
+        return mapper.convertToDto(
+                service.update(
+                        mapper.convertToEnable(editMan)));
     }
 
-    @RequestMapping(value = "mans/{id}", method = RequestMethod.DELETE)
-    public ManDTO delete(@PathVariable Long id) {
-        return mapper.convertToDto(service.delete(service.findById(id).get()));
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String delete(@PathVariable Long id) {
+        try {
+            service.deleteById(id);
+            return "Успешно";
+        }catch (NotFoundException e){
+            return "Не найдено";
+        }
+//        return mapper.convertToDto(service.delete(service.findById(id).get()));
     }
 
-//    @RequestMapping(value = "/mans",method = RequestMethod.GET)
+    @RequestMapping(value = "/count",method = RequestMethod.GET)
     public long getNumElemDB(){
-        return service.getNumElemBD();
+        return service.count();
     }
-
 }
